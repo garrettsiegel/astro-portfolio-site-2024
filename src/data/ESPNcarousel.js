c456d636967b63dc24416471a8b09202d7edd85e
garrettsiegel <garrett@submurgedcreative.com>
2024-01-15 10:24:37 -0500

    Carousel component updates

---
+++ src/data/ESPNcarousel.js
@@ -0,0 +1,20 @@
+const ESPNcarousel = [
+    {
+      src: "https://res.cloudinary.com/drxro4uha/image/upload/v1705329672/espn-year-in-review23_r9ej3m.jpg",
+      alt: "ESPN Year in Review 2023",
+      title: "Search Bar Trivia 2023",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla quidem iste vero expedita velit dignissimos fugit minima architecto, debitis, asperiores neque dolorum dolore commodi, mollitia tempora earum ab. Dolores quidem omnis odit voluptatibus esse fuga mollitia commodi quis deleniti eum dolorum, aperiam labore voluptatem reprehenderit vel nam veritatis. Nam, deserunt!",
+      link: "https://www.espn.com/espn/feature/story/_/id/39021244/trending-sports-moments-2023",
+      linkText: "See the Year in Review Quiz"
+    },
+    {
+      src: "https://res.cloudinary.com/drxro4uha/image/upload/v1697894718/brock1_16x9_rkmyzk.jpg",
+      alt: "Brock Purdy Salary",
+      title: "How quickly do 31 QBs earn Purdy's $870K?",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla quidem iste vero expedita velit dignissimos fugit minima architecto, debitis, asperiores neque dolorum dolore commodi, mollitia tempora earum ab. Dolores quidem omnis odit voluptatibus esse fuga mollitia commodi quis deleniti eum dolorum, aperiam labore voluptatem reprehenderit vel nam veritatis. Nam, deserunt!",
+      link: "https://www.espn.com/espn/feature/story/_/id/38512080/nfl-quarterbacks-earn-san-francisco-49ers-brock-purdy-salary",
+      linkText: "See the Brockulator"
+    },
+];
+
+export default ESPNcarousel;
