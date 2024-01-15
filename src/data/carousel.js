3be0a13f6791558365f32b9938fa74861ea86606
garrettsiegel <garrett@submurgedcreative.com>
2024-01-15 01:23:07 -0500

    Carousel functionality edits

---
+++ src/data/carousel.js
@@ -0,0 +1,22 @@
+const carousel = [
+    {
+      src: "https://picsum.photos/800/600?image=11",
+      alt: "First slide",
+      title: "First slide label",
+      desc: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+      src: "https://picsum.photos/800/600?image=12",
+      alt: "Second slide",
+      title: "Second slide label",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      src: "https://picsum.photos/800/600?image=13",
+      alt: "Third slide",
+      title: "Third slide label",
+      desc: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+    }
+];
+
+export default carousel;
