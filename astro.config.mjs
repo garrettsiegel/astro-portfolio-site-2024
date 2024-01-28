cdc8c755b4919c1e9e1b697985ecdd4ef1bb5e0c
garrettsiegel <garrett@submurgedcreative.com>
2024-01-28 00:39:55 -0500

    Revert to test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
 
-export default defineConfig({
-  integrations: [svelte({ preprocess: [] })],
-});
+// https://astro.build/config
+export default defineConfig({});
